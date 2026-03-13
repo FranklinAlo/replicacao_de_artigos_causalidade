aabcdb00d00c925aa344a3a40b4031e133ed657d
FranklinAlo <franklinalonso12345@gmail.com>
2026-03-13 18:48:08 -0300

    Versão 2.0

--- Artigo RDD - Thistlewaite, Campbell - Recriando resultados.R
+++ Artigo RDD - Thistlewaite, Campbell - Recriando resultados.R
@@ -9,23 +9,31 @@ df_merit <- dados_originais[12:21, ] #Dataframe com os dados dos alunos que rece
 
 #Figura 2: calculando as regressões GG e HH
 funcao_gg_commended <- lm(scholarship ~ test_score, data = df_commended)
+coeficiente_ggc <- as.numeric(coef(funcao_gg_commended)[2])
+intercepto_ggc <- as.numeric(coef(funcao_gg_commended)[1])
 funcao_gg_merit <- lm(scholarship ~ test_score, data = df_merit)
+coeficiente_ggm <- as.numeric(coef(funcao_gg_merit)[2])
+intercepto_ggm <- as.numeric(coef(funcao_gg_merit)[1])
 
 funcao_hh_commended <- lm(scholarship_150 ~ test_score, data = df_commended)
+coeficiente_hhc <- as.numeric(coef(funcao_hh_commended)[2])
+intercepto_hhc <- as.numeric(coef(funcao_hh_commended)[1])
 funcao_hh_merit <- lm(scholarship_150 ~ test_score, data = df_merit)
+coeficiente_hhm <- as.numeric(coef(funcao_hh_merit)[2])
+intercepto_hhm <- as.numeric(coef(funcao_hh_merit)[1])
 
 #Figura 2: grafando as regressões GG e HH
 ggplot(data = dados_originais, mapping = aes(x = test_score)) + #Usa dados_originais para colocar todos os pontos
   geom_point(aes(y = scholarship), color = 'blue') +
-  geom_segment(aes(x = 1, y = coef(funcao_gg_commended)[1], xend = 10, yend = coef(funcao_gg_commended)[2]*10+coef(funcao_gg_commended)[1]), linetype = 'dashed') + #Grafa a regressão GG de 0 a 10
-  geom_text(aes(x = 1, y = coef(funcao_gg_commended)[1] + 1, label = 'G'), check_overlap = TRUE) + #Adiciona o rótulo G para nomear a função de 0 a 10
-  geom_segment(aes(x = 11, y = coef(funcao_gg_merit)[2]*11+coef(funcao_gg_merit)[1], xend = 20, yend = coef(funcao_gg_merit)[2]*20+coef(funcao_gg_merit)[1]), linetype = 'dashed') + #Grafa a regressão GG de 11 a 20
-  geom_text(aes(x = 20, y = coef(funcao_gg_merit)[2]*20+coef(funcao_gg_merit)[1] + 1, label = "G'"), check_overlap = TRUE) + #Adiciona o rótulo G' para nomear a função de 11 a 20
+  geom_segment(aes(x = 1, y = intercepto_ggc, xend = 10, yend = coeficiente_ggc*10+intercepto_ggc), linetype = 'dashed') + #Grafa a regressão GG de 0 a 10
+  geom_text(aes(x = 1, y = intercepto_ggc + 1, label = 'G'), check_overlap = TRUE) + #Adiciona o rótulo G para nomear a função de 0 a 10
+  geom_segment(aes(x = 11, y = coeficiente_ggm*11+intercepto_ggm, xend = 20, yend = coeficiente_ggm*20+intercepto_ggm), linetype = 'dashed') + #Grafa a regressão GG de 11 a 20
+  geom_text(aes(x = 20, y = coeficiente_ggm*20+intercepto_ggm + 1, label = "G'"), check_overlap = TRUE) + #Adiciona o rótulo G' para nomear a função de 11 a 20
   geom_point(aes(y = scholarship_150), color = 'red') +
-  geom_segment(aes(x = 1, y = coef(funcao_hh_commended)[1], xend = 10, yend = coef(funcao_hh_commended)[2]*10+coef(funcao_hh_commended)[1]), linetype = 'dashed') + #Grafa a regressão HH de 0 a 10
-  geom_text(aes(x = 1, y = coef(funcao_hh_commended)[1] + 1, label = 'H'), check_overlap = TRUE) + #Adiciona o rótulo H para nomear a função de 0 a 10
-  geom_segment(aes(x = 11, y = coef(funcao_hh_merit)[2]*11+coef(funcao_hh_merit)[1], xend = 20, yend = coef(funcao_hh_merit)[2]*20+coef(funcao_hh_merit)[1]), linetype = 'dashed') + #Grafa a regressão HH de 11 a 20
-  geom_text(aes(x = 20, y = coef(funcao_hh_merit)[2]*20+coef(funcao_hh_merit)[1] + 1, label = "H'"), check_overlap = TRUE) + #Adiciona o rótulo H para nomear a função de 11 a 20
+  geom_segment(aes(x = 1, y = intercepto_hhc, xend = 10, yend = coeficiente_hhc*10+intercepto_hhc), linetype = 'dashed') + #Grafa a regressão HH de 0 a 10
+  geom_text(aes(x = 1, y = intercepto_hhc + 1, label = 'H'), check_overlap = TRUE) + #Adiciona o rótulo H para nomear a função de 0 a 10
+  geom_segment(aes(x = 11, y = coeficiente_hhm*11+intercepto_hhm, xend = 20, yend = coeficiente_hhm*20+intercepto_hhm), linetype = 'dashed') + #Grafa a regressão HH de 11 a 20
+  geom_text(aes(x = 20, y = coeficiente_hhm*20+intercepto_hhm + 1, label = "H'"), check_overlap = TRUE) + #Adiciona o rótulo H para nomear a função de 11 a 20
   geom_vline(xintercept = 10.5) + #Coloca a reta para definir o limite e separar os alunos commended dos merit
   scale_x_continuous(breaks = seq(1, 20, 1), limits = c(1, 20), minor_breaks = NULL) + #Ajusta o eixo x para ser igual ao do artigo
   scale_y_continuous(breaks = seq(42, 70, 4), limits = c(41, 70)) + #Ajusta o eixo y
@@ -36,23 +44,31 @@ ggplot(data = dados_originais, mapping = aes(x = test_score)) + #Usa dados_origi
 
 #Figura 3: calculando as regressões II e JJ
 funcao_ii_commended <- lm(graduate_study ~ test_score, data = df_commended)
+coeficiente_iic <- as.numeric(coef(funcao_ii_commended)[2])
+intercepto_iic <- as.numeric(coef(funcao_ii_commended)[1])
 funcao_ii_merit <- lm(graduate_study ~ test_score, data = df_merit)
+coeficiente_iim <- as.numeric(coef(funcao_ii_merit)[2])
+intercepto_iim <- as.numeric(coef(funcao_ii_merit)[1])
 
 funcao_jj_commended <- lm(teacher_researcher ~ test_score, data = df_commended)
+coeficiente_jjc <- as.numeric(coef(funcao_jj_commended)[2])
+intercepto_jjc <- as.numeric(coef(funcao_jj_commended)[1])
 funcao_jj_merit <- lm(teacher_researcher ~ test_score, data = df_merit)
+coeficiente_jjm <- as.numeric(coef(funcao_jj_merit)[2])
+intercepto_jjm <- as.numeric(coef(funcao_jj_merit)[1])
 
 #Figura 3: grafando as regressões II e JJ
 ggplot(data = dados_originais, mapping = aes(x = test_score)) +
   geom_point(aes(y = graduate_study), color = 'blue') +
-  geom_segment(aes(x = 1, y = coef(funcao_ii_commended)[1], xend = 10, yend = coef(funcao_ii_commended)[2]*10+coef(funcao_ii_commended)[1]), linetype = 'dashed') +
-  geom_text(aes(x = 1, y = coef(funcao_ii_commended)[1] + 1, label = 'I'), check_overlap = TRUE) +
-  geom_segment(aes(x = 11, y = coef(funcao_ii_merit)[2]*11+coef(funcao_ii_merit)[1], xend = 20, yend = coef(funcao_ii_merit)[2]*20+coef(funcao_ii_merit)[1]), linetype = 'dashed') +
-  geom_text(aes(x = 20, y = coef(funcao_ii_merit)[2]*20+coef(funcao_ii_merit)[1] + 1, label = "I'"), check_overlap = TRUE) +
+  geom_segment(aes(x = 1, y = intercepto_iic, xend = 10, yend = coeficiente_iic*10+intercepto_iic), linetype = 'dashed') +
+  geom_text(aes(x = 1, y = intercepto_iic + 1, label = 'I'), check_overlap = TRUE) +
+  geom_segment(aes(x = 11, y = coeficiente_iim*11+intercepto_iim, xend = 20, yend = coeficiente_iim*20+intercepto_iim), linetype = 'dashed') +
+  geom_text(aes(x = 20, y = coeficiente_iim*20+intercepto_iim + 1, label = "I'"), check_overlap = TRUE) +
   geom_point(aes(y = teacher_researcher), color = 'red') +
-  geom_segment(aes(x = 1, y = coef(funcao_jj_commended)[1], xend = 10, yend = coef(funcao_jj_commended)[2]*10+coef(funcao_jj_commended)[1]), linetype = 'dashed') +
-  geom_text(aes(x = 1, y = coef(funcao_jj_commended)[1] - 1, label = 'J'), check_overlap = TRUE) +
-  geom_segment(aes(x = 11, y = coef(funcao_jj_merit)[2]*11+coef(funcao_jj_merit)[1], xend = 20, yend = coef(funcao_jj_merit)[2]*20+coef(funcao_jj_merit)[1]), linetype = 'dashed') +
-  geom_text(aes(x = 20, y = coef(funcao_jj_merit)[2]*20+coef(funcao_jj_merit)[1] - 1, label = "J'"), check_overlap = TRUE) +
+  geom_segment(aes(x = 1, y = intercepto_jjc, xend = 10, yend = coeficiente_jjc*10+intercepto_jjc), linetype = 'dashed') +
+  geom_text(aes(x = 1, y = intercepto_jjc - 1, label = 'J'), check_overlap = TRUE) +
+  geom_segment(aes(x = 11, y = coeficiente_jjm*11+intercepto_jjm, xend = 20, yend = coeficiente_jjm*20+intercepto_jjm), linetype = 'dashed') +
+  geom_text(aes(x = 20, y = coeficiente_jjm*20+intercepto_jjm - 1, label = "J'"), check_overlap = TRUE) +
   geom_vline(xintercept = 10.5) +
   scale_x_continuous(breaks = seq(1, 20, 1), limits = c(1, 20), minor_breaks = NULL) +
   scale_y_continuous(breaks = seq(18, 46, 4), limits = c(17, 46)) +
@@ -63,15 +79,19 @@ ggplot(data = dados_originais, mapping = aes(x = test_score)) +
 
 #Figura 4: calculando a regressão KK
 funcao_kk_commended <- lm(intellectualism ~ test_score, data = df_commended)
+coeficiente_kkc <- as.numeric(coef(funcao_kk_commended)[2])
+intercepto_kkc <- as.numeric(coef(funcao_kk_commended)[1])
 funcao_kk_merit <- lm(intellectualism ~ test_score, data = df_merit)
+coeficiente_kkm <- as.numeric(coef(funcao_kk_merit)[2])
+intercepto_kkm <- as.numeric(coef(funcao_kk_merit)[1])
 
 #Figura 4: grafando a regressão KK
 ggplot(data = dados_originais, mapping = aes(x = test_score)) +
   geom_point(aes(y = intellectualism), color = 'blue') +
-  geom_segment(aes(x = 1, y = coef(funcao_kk_commended)[1], xend = 10, yend = coef(funcao_kk_commended)[2]*10+coef(funcao_kk_commended)[1]), linetype = 'dashed') +
-  geom_text(aes(x = 1, y = coef(funcao_kk_commended)[1] + 0.04, label = 'K'), check_overlap = TRUE) +
-  geom_segment(aes(x = 11, y = coef(funcao_kk_merit)[2]*11+coef(funcao_kk_merit)[1], xend = 20, yend = coef(funcao_kk_merit)[2]*20+coef(funcao_kk_merit)[1]), linetype = 'dashed') +
-  geom_text(aes(x = 20, y = coef(funcao_kk_merit)[2]*20+coef(funcao_kk_merit)[1] - 0.04, label = "K'"), check_overlap = TRUE) +
+  geom_segment(aes(x = 1, y = intercepto_kkc, xend = 10, yend = coeficiente_kkc*10+intercepto_kkc), linetype = 'dashed') +
+  geom_text(aes(x = 1, y = intercepto_kkc + 0.04, label = 'K'), check_overlap = TRUE) +
+  geom_segment(aes(x = 11, y = coeficiente_kkm*11+intercepto_kkm, xend = 20, yend = coeficiente_kkm*20+intercepto_kkm), linetype = 'dashed') +
+  geom_text(aes(x = 20, y = coeficiente_kkm*20+intercepto_kkm - 0.04, label = "K'"), check_overlap = TRUE) +
   geom_vline(xintercept = 10.5) +
   scale_x_continuous(breaks = seq(1, 20, 1), limits = c(1, 20), minor_breaks = NULL) +
   scale_y_continuous(breaks = seq(3.63, 4.11, 0.08), limits = c(3.60, 4.11)) +
